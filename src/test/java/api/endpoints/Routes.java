cd0c56a0c33fda0ab902d6cd42d11481edd03153
Anton Vinokurov <anton.vinokurov.qa@gmail.com>
2024-01-31 16:53:52 +0100

    add Routes and UserEndPoints

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,14 @@
+package api.endpoints;
+
+/**
+ * Base URL --> petstore.swagger.io/v2
+ */
+public class Routes {
+    public static String baseUrl = "https://petstore.swagger.io/v2";
+
+    //user module
+    public static String postUrl = baseUrl + "/user";
+    public static String getUrl = baseUrl + "/user/{username}";
+    public static String updateUrl = baseUrl + "/user/{username}";
+    public static String deleteUrl = baseUrl + "/user/{username}";
+}
